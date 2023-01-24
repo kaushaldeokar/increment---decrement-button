e625cd5abbaf1b58136de91175a29a59764d133b
kaushaldeokar <kaushal.deokar@gmail.com>
2023-01-24 12:54:16 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import NavBar from './component/NavBar';
+import React,{useState} from 'react';
 function App() {
+  const [num,setNum]=useState(1);
+  
+  function inc(){
+    setNum(num+1);
+  }
+  function dec(){
+    setNum(num-1);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <div className="container">
+        <h1>{num}</h1>
+        <div className="buttons">
+            <button type="button" class="btn btn-dark" onClick={inc}>Increment</button>
+            <button type="button" class="btn btn-dark"  onClick={dec}>Decrement</button>
+        </div>
+      </div>
     </div>
   );
 }
